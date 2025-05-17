2e766e24e6ddd678eb6a1a4196ea2fd25ff486a0
Djalma <djalmahy@hotmail.com>
2025-05-16 21:57:30 -0300

    Feat: created footer and header.

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>Made by Djalma Hideki Yamamoto</p>
+        </footer>
+    );
+}
--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../Header';
 import Navbar from '../Navbar';
+import Footer from '../Footer';
 import './styles.css';
 
 export default function Layout() {
     return (
         <div className="layout">
-            <Navbar />
-            <Outlet />
+            <Header />
+            <div className="layout-content">
+                <Navbar />
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 }
