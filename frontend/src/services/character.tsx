47df6e51d51aaae340f97a7d19c6cad06f8b56d1
Djalma <djalmahy@hotmail.com>
2025-06-11 12:32:34 -0300

    Feat: added character attributes

--- frontend/src/services/character.tsx
+++ frontend/src/services/character.tsx
@@ -1,7 +1,45 @@
+export interface StatModifier {
+    flat: number;
+    percent: number;
+    multiplier: number;
+}
+
+export interface ManualModifier {
+    flat: number;
+    percent: number;
+    multiplier: number;
+}
+
 export interface AcquiredTalent {
     id: string;
     level: number;
 }
+
+export interface Attribute {
+    base: number;
+    modifiers: StatModifier;
+    manual: ManualModifier;
+}
+
+export type AttributeKey =
+    | 'hp'
+    | 'mp'
+    | 'ki'
+    | 'barrier'
+    | 'armor'
+    | 'strength'
+    | 'dexterity'
+    | 'vigor'
+    | 'senses'
+    | 'charisma'
+    | 'mind'
+    | 'spirit'
+    | 'luck'
+    | 'actions'
+    | 'speed'
+    | 'flyingSpeed';
+
+export type AttributeMap = Record<AttributeKey, Attribute>;
 
 export interface Character {
     id: number;
@@ -10,4 +48,5 @@ export interface Character {
     spentClp: number;
     acquiredTalents: Record<number, AcquiredTalent[]>;
     classInvestments: Record<number, number>;
+    attributes: AttributeMap;
 }
