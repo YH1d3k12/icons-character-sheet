9c0006fa216b0ad13f1b2fd5f6765e064b6d7300
Djalma <djalmahy@hotmail.com>
2025-06-16 21:53:14 -0300

    WIP: resistances component.

--- frontend/src/services/character.tsx
+++ frontend/src/services/character.tsx
@@ -1,26 +1,5 @@
-export interface StatModifier {
-    flat: number;
-    percent: number;
-    multiplier: number;
-}
-
-export interface ManualModifier {
-    flat: number;
-    percent: number;
-    multiplier: number;
-}
-
-export interface AcquiredTalent {
-    id: string;
-    level: number;
-}
-
-export interface Attribute {
-    base: number;
-    modifiers: StatModifier;
-    manual: ManualModifier;
-}
-
+export type ModifierType = 'flat' | 'percent' | 'multiplier';
+export type ModifierSource = 'manual' | 'system';
 export type AttributeKey =
     | 'hp'
     | 'mp'
@@ -38,15 +17,56 @@ export type AttributeKey =
     | 'actions'
     | 'speed'
     | 'flyingSpeed';
+export type ResistanceKey =
+    | 'slashing'
+    | 'piercing'
+    | 'bludgeoning'
+    | 'corrosive'
+    | 'cold'
+    | 'eletric'
+    | 'fire'
+    | 'thauma'
+    | 'thunder'
+    | 'poison'
+    | 'psychic'
+    | 'aetheric'
+    | 'light'
+    | 'dark';
+
+export interface AcquiredTalent {
+    id: string;
+    level: number;
+}
+
+export interface Attribute {
+    base: number;
+    modifiers: {
+        value: number;
+        type: ModifierType;
+        source: ModifierSource;
+    }[];
+}
+
+export interface Resistance {
+    base: number;
+    modifiers: {
+        value: number;
+        type: ModifierType;
+        source: ModifierSource;
+    }[];
+}
 
 export type AttributeMap = Record<AttributeKey, Attribute>;
+export type ResistanceMap = Record<ResistanceKey, Resistance>;
 
 export interface Character {
     id: number;
+    level: number;
     name: string;
     maxClp: number;
     spentClp: number;
     acquiredTalents: Record<number, AcquiredTalent[]>;
     classInvestments: Record<number, number>;
     attributes: AttributeMap;
+    resistances: ResistanceMap;
 }
