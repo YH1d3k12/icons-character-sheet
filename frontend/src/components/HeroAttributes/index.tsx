dd1a68a5ddbc4d238d0eff1641368767303df15d
Djalma <djalmahy@hotmail.com>
2025-07-21 14:00:55 -0300

    chore: added plus symbol to positive hero attributes.

--- frontend/src/components/HeroAttributes/index.tsx
+++ frontend/src/components/HeroAttributes/index.tsx
@@ -18,28 +18,52 @@ export default function HeroAttributes() {
                     <h3>Strength</h3>
                     <p>
                         {strength.base}
-                        <span>({strength.modifier})</span>
+                        <span>
+                            (
+                            {strength.modifier > 0
+                                ? '+' + strength.modifier
+                                : strength.modifier}
+                            )
+                        </span>
                     </p>
                 </div>
                 <div className="hero-section-attribute">
                     <h3>Dexterity</h3>
                     <p>
                         {dexterity.base}
-                        <span>({dexterity.modifier})</span>
+                        <span>
+                            (
+                            {dexterity.modifier > 0
+                                ? '+' + dexterity.modifier
+                                : dexterity.modifier}
+                            )
+                        </span>
                     </p>
                 </div>
                 <div className="hero-section-attribute">
                     <h3>Vigor</h3>
                     <p>
                         {vigor.base}
-                        <span>({vigor.modifier})</span>
+                        <span>
+                            (
+                            {vigor.modifier > 0
+                                ? '+' + vigor.modifier
+                                : vigor.modifier}
+                            )
+                        </span>
                     </p>
                 </div>
                 <div className="hero-section-attribute">
                     <h3>Senses</h3>
                     <p>
                         {senses.base}
-                        <span>({senses.modifier})</span>
+                        <span>
+                            (
+                            {senses.modifier > 0
+                                ? '+' + senses.modifier
+                                : senses.modifier}
+                            )
+                        </span>
                     </p>
                 </div>
             </div>
@@ -48,28 +72,52 @@ export default function HeroAttributes() {
                     <h3>Charisma</h3>
                     <p>
                         {charisma.base}
-                        <span>({charisma.modifier})</span>
+                        <span>
+                            (
+                            {charisma.modifier > 0
+                                ? '+' + charisma.modifier
+                                : charisma.modifier}
+                            )
+                        </span>
                     </p>
                 </div>
                 <div className="hero-section-attribute">
                     <h3>Mind</h3>
                     <p>
                         {mind.base}
-                        <span>({mind.modifier})</span>
+                        <span>
+                            (
+                            {mind.modifier > 0
+                                ? '+' + mind.modifier
+                                : mind.modifier}
+                            )
+                        </span>
                     </p>
                 </div>
                 <div className="hero-section-attribute">
                     <h3>Spirit</h3>
                     <p>
                         {spirit.base}
-                        <span>({spirit.modifier})</span>
+                        <span>
+                            (
+                            {spirit.modifier > 0
+                                ? '+' + spirit.modifier
+                                : spirit.modifier}
+                            )
+                        </span>
                     </p>
                 </div>
                 <div className="hero-section-attribute">
                     <h3>Luck</h3>
                     <p>
                         {luck.base}
-                        <span>({luck.modifier})</span>
+                        <span>
+                            (
+                            {luck.modifier > 0
+                                ? '+' + luck.modifier
+                                : luck.modifier}
+                            )
+                        </span>
                     </p>
                 </div>
             </div>
